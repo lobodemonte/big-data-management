f6b104c37300795c2f765312670e833dd57c056b
Erik Lopez <erikl.von@gmail.com>
2020-03-30 20:01:27 -0400

    trying this again

--- Homework/HW3/BDM_HW3.py
+++ Homework/HW3/BDM_HW3.py
@@ -7,9 +7,10 @@ from pyspark import SparkContext
 from pyspark.sql.session import SparkSession
 
 def toCSVLine(entry):
-    if ',' in entry[0]:
-        entry[0] = '"' + entry[0] + '"'
-    return ','.join(str(elem) for elem in entry)
+    product = entry[0][0]
+    if ',' in entry[0][0]:
+        product = '"' + entry[0][0] + '"'
+    return product + "," + str(entry[0][1]) + "," + str(entry[1][0]) + "," + str(entry[1][1]) + "," + str(entry[1][2])
 
 def extract_complaints(partId, list_of_records):
     if partId==0: 
@@ -17,29 +18,34 @@ def extract_complaints(partId, list_of_records):
     import csv
     reader = csv.reader(list_of_records)
     for row in reader:
-        year_received = dt.datetime.strptime(row[0], '%m/%d/%Y').year
-        product = row[1].lower()
-        company = row[7].lower()            
-        yield ((product, year_received, company), 1)
+        try:
+            year_received = dt.datetime.strptime(row[0], '%Y-%m-%d').year
+            product = row[1].lower()
+            company = row[7].lower()            
+            yield ((product, year_received, company), 1)
+        except:
+            print("Exception Occurred", row)
 
 def run_spark(complaints_file_path, output_path):
     sc = SparkContext()
     spark = SparkSession(sc)
 
-    complaint_info = sc.textFile(input_file, use_unicode=True).cache()
+    print("Reading input file:", complaints_file_path)
+    complaint_info = sc.textFile(complaints_file_path, use_unicode=True).cache()
     print("Number of partitions: ", complaint_info.getNumPartitions())
 
     complaints = complaint_info.mapPartitionsWithIndex(extract_complaints)
 
-    temp = complaint_info.mapPartitionsWithIndex(extract_complaints) \
+    results = complaint_info.mapPartitionsWithIndex(extract_complaints) \
         .reduceByKey(lambda x,y: x+y) \
         .map(lambda x: ( (x[0][0],x[0][1]), [x[1]] ) ) \
         .reduceByKey(lambda x,y: x+y) \
         .map(lambda x: ( x[0], ( sum(x[1]), len(x[1]), round(max(x[1])*100/sum(x[1])) ) ) ) \
         .sortByKey() \
-        .map(lambda x: (x[0] + x[1]))
-
-    temp.saveAsTextFile(output_path)
+        .map(toCSVLine) \
+    
+    print("Saving to: ", output_path)
+    results.saveAsTextFile(output_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -47,7 +53,4 @@ if __name__ == '__main__':
     parser.add_argument("output_path", type=Path)
 
     p = parser.parse_args()
-    if (p.input_file.exists())):
-        run_spark(str(p.input_file), str(p.output_path))
-    else:
-        print("Input File Path: {}, Exists: {}".format(p.input_file, p.input_file.exists()))
+    run_spark(str(p.input_file), str(p.output_path))
