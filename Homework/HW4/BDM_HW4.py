a538c0580ed1af484f1d0ec19ccbfab5c331f886
Erik Lopez <erikl.von@gmail.com>
2020-04-23 01:13:11 -0400

    idk anymore

--- Homework/HW4/BDM_HW4.py
+++ Homework/HW4/BDM_HW4.py
@@ -8,20 +8,17 @@ boros_file = 'boroughs.geojson'
 neighborhood_file = 'neighborhoods.geojson'
 
 def genIndex(shapefile):
-    import rtree
     import fiona.crs
     import geopandas as gpd
     zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
-    index = rtree.Rtree()
     for idx, geometry in enumerate(zones.geometry):
         yield (idx, geometry.bounds, zones.iloc[idx])
 
 def getZone(p, index, field):
-    matches = index.intersection((p.x, p.y, p.x, p.y), objects=True)
+    matches = index.intersection((p.x, p.y, p.x, p.y), objects='raw')
     for match in matches:
-        hood = match.object
-        if hood.geometry.contains(p):
-            return hood[field]
+        if match.geometry.contains(p):
+            return match[field]
     return None
 
 def createIndex(shapefile):
@@ -30,7 +27,7 @@ def createIndex(shapefile):
     import geopandas as gpd
     zones = gpd.read_file(shapefile).to_crs(fiona.crs.from_epsg(2263))
     index = rtree.Rtree()
-    for idx,geometry in enumerate(zones.geometry):
+    for idx, geometry in enumerate(zones.geometry):
         index.insert(idx, geometry.bounds)
     return {"index": index, "zones": zones}
 
@@ -45,69 +42,69 @@ def processTrips(pid, records):
     import csv
     import pyproj
     import shapely.geometry as geom
+    import rtree.index
 
     # Skip the header
-    if pid==0:
+    if pid == 0:
         next(records)
     
     reader = csv.reader(records)
-    
-    # Create an R-tree index
     proj = pyproj.Proj(init="epsg:2263", preserve_units=True)    
     
-    #boros = createIndex(boros_file)    
-    #neighborhoods = createIndex(neighborhood_file)    
-    boros_index = rtree.index.Index(genIndex('boroughs.geojson'))
-    hood_index = rtree.index.Index(genIndex('neighborhoods.geojson'))
+    boros = createIndex(boros_file)    
+    neighborhoods = createIndex(neighborhood_file)    
+    #boros_gen = rtree.index.Index(genIndex('boroughs.geojson'))
+    #hood_gen = rtree.index.Index(genIndex('neighborhoods.geojson'))
 
     for row in reader:
         try: 
-            if 'NULL' in row[2:5]: 
-                continue
-            # if 'NULL' in row[5:6] or 'NULL' in row[9:10]:
+            # if 'NULL' in row[2:6]: 
             #     continue
+            if 'NULL' in row[5:7] or 'NULL' in row[9:11]:
+                continue
 
             pickup_point = geom.Point(proj(float(row[3]), float(row[2])))
-            # pickup_point = geom.Point(proj(float(row[5]), float(row[6])))            
+            dropoff_point= geom.Point(proj(float(row[5]), float(row[4])))
 
-#             start_boro = findZone(pickup_point, boros)
-            start_boro = getZone(pickup_point, boros_index, 'boroname')
-            
-            if start_boro:
-                dropoff_point= geom.Point(proj(float(row[5]), float(row[4])))
-                # dropoff_point= geom.Point(proj(float(row[9]), float(row[10])))
+            start_boro = findZone(pickup_point, boros)
+            end_hood = findZone(dropoff_point, neighborhoods)
+
+#             start_boro = getZone(pickup_point, boros_gen, 'boroname')
+#             end_hood = getZone(dropoff_point, hood_gen, 'neighborhood')
+
+            if start_boro and end_hood:
                 
-                # end_hood = findZone(dropoff_point, neighborhoods)
-                end_hood = getZone(dropoff_point, hood_index, 'neighborhood')
+                end_hood = findZone(dropoff_point, neighborhoods)
+                # end_hood = getZone(dropoff_point, hood_index, 'neighborhood')
                 
                 if end_hood:
-                    # boro_name = boros['zones'].iloc[start_boro]['boroname']
-                    # hood_name = neighborhoods['zones'].iloc[end_hood]['neighborhood']
-                    # yield ( (boro_name, hood_name), 1 )
-                    yield ( (start_boro, end_hood), 1 )
+                    boro_name = boros['zones'].iloc[start_boro]['boroname']
+                    hood_name = neighborhoods['zones'].iloc[end_hood]['neighborhood']
+                    yield ( (boro_name, hood_name), 1 )
+#                     yield ( (start_boro, end_hood), 1 )
 
         except: 
             print("Failed at: ", row) ## TODO this won't log anything
 
-def run_spark(taxi_file, output_path, sc):
+def run_spark(taxi_file, output_path):
     from heapq import nlargest
     from operator import itemgetter
-
+    
+    sc = SparkContext()
     rdd = sc.textFile(taxi_file).mapPartitionsWithIndex(processTrips)
     
     counts = rdd.reduceByKey(lambda x,y: x+y) \
                 .map(lambda x: ( x[0][0], [(x[0][1], x[1])] ) ) \
                 .reduceByKey(lambda x,y: x+y) \
                 .mapValues(lambda hood_counts: nlargest(3, hood_counts, key=itemgetter(1))) \
+                .sortByKey() \                 
                 .map(lambda x: str(x[0])+ "," + ",".join([str(i) for sub in x[1] for i in sub])) \
-                .sortByKey()
 
     counts.saveAsTextFile(output_path)
 
 if __name__ == '__main__':
     import argparse
     from pathlib import Path
-    sc = SparkContext()
     parser = argparse.ArgumentParser()
     
     parser.add_argument("input_file", type=Path)
@@ -116,7 +113,7 @@ if __name__ == '__main__':
 
     print("Input File: ", str(p.input_file))
     print("Output Path: ", str(p.output_path))
-    run_spark(str(p.input_file), str(p.output_path), sc)
+    run_spark(str(p.input_file), str(p.output_path))
     print("Done")
 
 
